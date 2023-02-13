dbd2d0aecee8553398006d3a1e2db9955f1c9600
Emre Uçar <emreucargithub@gmail.com>
2023-02-13 14:08:15 +0300

    React-Axios
    React-Axios mantığı ile veri çekme

---
+++ axios_veri_cekme/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+
+
+function App() {
+
+  const [veri, setVeri] = useState("");
+  const [id, setId] = useState(1);
+
+  useEffect(() => {
+
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(res => setVeri(res.data[id].body))
+    .catch(err => console.log(err))
+
+  }, [veri, id])
+
+
+  return (
+    <div className="App">
+      <h1> {veri} </h1>
+      <p>
+        <input type='text' value={id} onChange={e => setId(e.target.value)}/>
+      </p>    
+    </div>
+  );
+}
+
+export default App;
